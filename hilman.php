84d292b15b349b2a055a21c7d38fabe610710787
hilmanmutasimluton <hilmanmutasim26.com>
2023-12-10 13:46:16 +0700

    add file

---
+++ hilman.php
@@ -0,0 +1,9 @@
+<?php
+$nilai = 100;
+
+if ($nilai >= 70) {
+    echo "Selamat, Anda lulus (tapi copas) ";
+} else {
+    echo "Maaf, Anda belum lulus (tetap semangat berjuang allah suka hambanya yang tidak gampang menyerah!.";
+}
+?>
